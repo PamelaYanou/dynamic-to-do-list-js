c4940e3dacd3a83bd83ae64d52e0a4f99003a1e4
PamelaYanou <yanoupamela@gmail.com>
2024-10-12 15:18:13 +0200

    create index.html, script.js and styles.css files

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let addButton = document.getElementById("add-task-btn");
+  let taskInput = document.getElementById("task-input");
+  let taskList = document.getElementById("task-list");
+
+  function addTask() {
+  let taskText = taskInput.value.trim();
+  if (taskText === "") {
+  alert("Please enter a task.");
+  return; 
+    }
+    let taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.className = "remove-btn";
+
+    removeButton.onclick = function () {
+      taskList.removeChild(taskItem);
+    };
+    taskItem.appendChild(removeButton);    
+    taskList.appendChild(taskItem);  
+    taskInput.value = "";
+  }
+   addButton.addEventListener("click", addTask); 
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+});
